cdeb881804f6aebccfb74de35ee92541a59b8e69
Dane Brown <tain198127@163.com>
2023-05-25 23:15:10 +0800

    完成chatglm_6b_4int调试

--- torch/ChatGLM_6B.py
+++ torch/ChatGLM_6B.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#
+#写在最前
+#在mac上运行，先要
+# curl -O https://mac.r-project.org/openmp/openmp-14.0.6-darwin20-Release.tar.gz
+# sudo tar fvxz openmp-14.0.6-darwin20-Release.tar.gz -C /
+# 加载openmp才可以
+# 然后要安装 pip install -r requirements.txt
+#如果想用mac m1 max的mps 需要如下步骤：
+# curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
+# sh Miniconda3-latest-MacOSX-arm64.sh
+# conda install pytorch torchvision torchaudio -c pytorch-nightly
+
+#
+#
 import faulthandler
 faulthandler.enable()
 from transformers import AutoTokenizer, AutoModel
